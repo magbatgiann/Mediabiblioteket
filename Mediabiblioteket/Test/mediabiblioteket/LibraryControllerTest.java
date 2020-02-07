1055f3815f12dd0f3eb626576eaba526fe319ef8
magbatgiann <manioulka@gmail.com>
2020-02-07 09:28:47 +0100

    user input check test

---
+++ Mediabiblioteket/Test/mediabiblioteket/LibraryControllerTest.java
@@ -0,0 +1,77 @@
+package mediabiblioteket;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryControllerTest {
+LibraryController controller = new LibraryController();
+
+    @Test
+    void checkUserInput() {
+        boolean test = controller.checkUserInput("dvd");
+        assertEquals(true,test);
+    }
+    @Test
+    void checkUserInputFalse() {
+        boolean test = controller.checkUserInput("!#%");
+        assertEquals(false,test);
+    }
+
+    @Test
+    void checkInputOnlyDigits() {
+
+    }
+
+    @Test
+    void writeToFile() {
+    }
+
+    @Test
+    void borrowMedia() {
+    }
+
+    @Test
+    void returnMedia() {
+    }
+
+    @Test
+    void checkIfBorrowerExist() {
+    }
+
+    @Test
+    void sortMedia() {
+    }
+
+    @Test
+    void getMedia() {
+    }
+
+    @Test
+    void showSelectedMediaInfo() {
+    }
+
+    @Test
+    void searchMediaAllByString() {
+    }
+
+    @Test
+    void getMediaFromSearchResult() {
+    }
+
+    @Test
+    void searchMediaTitleByString() {
+    }
+
+    @Test
+    void getBorrower() {
+    }
+
+    @Test
+    void loadBorrowedMedia() {
+    }
+
+    @Test
+    void searchBorrowed() {
+    }
+}
